18a4bcd0b65492a738281a68b89dd5558118ab84
Aftab Iqbal <aftabiq@gmail.com>
2011-06-22 20:49:22 +0100

    Statistical chart using json data. Sample json data is in test.js

---
+++ sandbox/test.js
@@ -0,0 +1,6 @@
+{"data": [
+{"schoolGrade":{"type":"uri","value":"http://data-gov.ie/SchoolGrade/All_Excluding_TY"},"numberOfStudents":{"datatype":"http://www.w3.org/2001/XMLSchema#integer","type":"typed-literal","value":"209"}},
+{"schoolGrade":{"type":"uri","value":"http://data-gov.ie/SchoolGrade/FirstYear"},"numberOfStudents":{"datatype":"http://www.w3.org/2001/XMLSchema#integer","type":"typed-literal","value":"34"}},
+{"schoolGrade":{"type":"uri","value":"http://data-gov.ie/SchoolGrade/ThirdYear"},"numberOfStudents":{"datatype":"http://www.w3.org/2001/XMLSchema#integer","type":"typed-literal","value":"52"}},
+{"schoolGrade":{"type":"uri","value":"http://data-gov.ie/SchoolGrade/SecondYear"},"numberOfStudents":{"datatype":"http://www.w3.org/2001/XMLSchema#integer","type":"typed-literal","value":"37"}}
+]}
